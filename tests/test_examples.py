03b9e71e372322da67b8b811f7bb21b9077f2c62
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-10-27 13:40:53 +0200

    Add simple opendrift example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    pass
+def test_simple_example():
+    from datetime import datetime, timedelta
+    from opendrift.models.oceandrift import OceanDrift
+
+    o = OceanDrift(loglevel=20)
+    o.set_config('environment:fallback:x_sea_water_velocity', 0)
+    o.set_config('environment:fallback:y_sea_water_velocity', 1)
+    o.set_config('environment:fallback:land_binary_mask', 0)
+
+    o.seed_elements(lon=4.0, lat=60.0, radius=0, number=1, time=datetime(2015, 1, 1), radius_type='uniform')
+
+    o.run(duration=timedelta(hours=1))
+
+    assert o.elements.lat[0] > 60
+    assert o.elements.lon[0] == 4
